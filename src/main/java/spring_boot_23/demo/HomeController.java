94d32f3ff5ca994b7eba94cfee0d5eb4fd5e1758
Diwakar Sharma <diwakar2034@yahoo.com>
2021-04-01 22:46:43 -0400

    initial commit

---
+++ src/main/java/spring_boot_23/demo/HomeController.java
@@ -0,0 +1,23 @@
+package spring_boot_23.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return  "index";
+    }
+
+    @RequestMapping("/2")
+    public String page2(){
+        return "page2";
+    }
+
+  /*  @RequestMapping("/error")
+    public String Error(){
+        return "error";
+    }*/
+
+}
